4fb8eb698c0ce359dfd91c930478510fc2f44d6e
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-06-26 10:16:24 -0300

    bugfix

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,4 +1,4 @@
 import { app } from "./server";
 
 const port = process.env.PORT || 8080;
-app.listen(8080, () => console.log(`Servidor rodando na porta ${port}`));
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
